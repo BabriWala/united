beb5214feb49ac9c458cf7f12c29a6eb1dd7bd6c
Md. Anower Hossan <anowerhossan1988@gmail.com>
2024-05-16 22:35:50 +0600

    home page mobile responsive done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,18 @@
 
-// back to top
+
 $(document).ready(function(){
-    $(window).scroll(function () {
-      if ($(this).scrollTop() > 100) {
-        $(".back_to_top").css({ transform: "translateY(0)" });
-      } else {
-        $(".back_to_top").css({ transform: "translateY(100px)" });
-      }
-    });
+  // back to top
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $(".back_to_top").css({ transform: "translateY(0)" });
+    } else {
+      $(".back_to_top").css({ transform: "translateY(100px)" });
+    }
+  });
+
+  // mobile menu
+  $(".mobile_inner_div").click(function(){
+    $(this).next(".mobile_sub_list").toggleClass("display_block");
+    $(this).find("i").toggleClass("rotate_180");
+  });
 });
