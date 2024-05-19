1e06c0fdfb262b910682671a412167a3fb9d3f62
BabriWala <hanzalafarhann@gmail.com>
2024-05-19 06:19:18 +0600

    career page done with responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,11 @@
 
-
+// back to top
 $(document).ready(function(){
-  // back to top
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > 100) {
-      $(".back_to_top").css({ transform: "translateY(0)" });
-    } else {
-      $(".back_to_top").css({ transform: "translateY(100px)" });
-    }
-  });
-
-  // mobile menu
-  $(".mobile_inner_div").click(function(){
-    $(this).next(".mobile_sub_list").toggleClass("display_block");
-    $(this).find("i").toggleClass("rotate_180");
-  });
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 100) {
+        $(".back_to_top").css({ transform: "translateY(0)" });
+      } else {
+        $(".back_to_top").css({ transform: "translateY(100px)" });
+      }
+    });
 });
