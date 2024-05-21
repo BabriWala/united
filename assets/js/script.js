851e5bd56e096e21d9e9b9340ab7bd43e1b62620
BabriWala <hanzalafarhann@gmail.com>
2024-05-21 06:08:20 +0600

    home page done

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@
   "use strict";
 
   $(document).ready(function () {
+    new WOW().init();
     (function () {
       const UG_APPLICATION_SUBMITS = document.querySelectorAll(
         ".ug-application-submit"
@@ -19,25 +20,26 @@
       maxWidth: 1920,
       maxHeight: 1080,
     });
-    var wow = new WOW({
-      boxClass: "wow",
-      // animated element css class (default is wow)
-      animateClass: "animated",
-      // animation css class (default is animated)
-      offset: 0,
-      // distance to the element when triggering the animation (default is 0)
-      mobile: true,
-      // trigger animations on mobile devices (default is true)
-      live: true,
-      // act on asynchronously loaded content (default is true)
-      callback: function (box) {
-        // the callback is fired every time an animation is started
-        // the argument that is passed in is the DOM node being animated
-      },
-      scrollContainer: null, // optional scroll container selector, otherwise use window
-    });
 
-    wow.init();
+    // var wow = new WOW({
+    //   boxClass: "wow",
+    //   // animated element css class (default is wow)
+    //   animateClass: "animated",
+    //   // animation css class (default is animated)
+    //   offset: 0,
+    //   // distance to the element when triggering the animation (default is 0)
+    //   mobile: true,
+    //   // trigger animations on mobile devices (default is true)
+    //   live: true,
+    //   // act on asynchronously loaded content (default is true)
+    //   callback: function (box) {
+    //     // the callback is fired every time an animation is started
+    //     // the argument that is passed in is the DOM node being animated
+    //   },
+    //   scrollContainer: null, // optional scroll container selector, otherwise use window
+    // });
+    // wow.init();
+    console.log("wow js");
     $(".animated-countdown").counterUp({
       delay: 10,
       time: 1000,
